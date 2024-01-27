2c58b27712b483128fa3830d6963f61d6508e490
Poorshad Shaddel <p.shaddel@gmail.com>
2024-01-27 17:47:59 +0000

    we should return error when the gitjs folder is already there and we want to initate the folder again

--- src/init.js
+++ src/init.js
@@ -1,17 +1,24 @@
 const fs = require("fs");
 
-function init() {
-    // creates a folder called .gitj and creates a subfolder called .gitj/objects and .gitj/refs and .gitj/refs/heads
-    fs.mkdirSync(".gitj");
-    fs.mkdirSync(".gitj/objects");
-    fs.mkdirSync(".gitj/refs");
-    fs.mkdirSync(".gitj/refs/heads");
-    // creates a file called .gitj/refs/heads/master
-    fs.writeFileSync(".gitj/refs/heads/master", "");
-    // creates a file called .gitj/HEAD
-    fs.writeFileSync(".gitj/HEAD", "ref: refs/heads/master");
-    // index file to store staged files
-    fs.writeFileSync(".gitj/index", "");
+function init(folder = './') {
+    // return error if the .gitj folder already exists
+    try {
+        fs.accessSync(folder + ".gitj");
+        console.log("Git repository already initialized.");
+        process.exit(1);
+    } catch (error) {
+        // creates a folder called .gitj and creates a subfolder called .gitj/objects and .gitj/refs and .gitj/refs/heads
+        fs.mkdirSync(folder + ".gitj");
+        fs.mkdirSync(folder + ".gitj/objects");
+        fs.mkdirSync(folder + ".gitj/refs");
+        fs.mkdirSync(folder + ".gitj/refs/heads");
+        // creates a file called .gitj/refs/heads/master
+        fs.writeFileSync(folder + ".gitj/refs/heads/master", "");
+        // creates a file called .gitj/HEAD
+        fs.writeFileSync(folder + ".gitj/HEAD", "ref: refs/heads/master");
+        // index file to store staged files
+        fs.writeFileSync(folder + ".gitj/index", "");
+    }
 }
 
 module.exports = { init };
